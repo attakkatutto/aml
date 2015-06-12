f341eae5335fe3d3bfe77d0b3e4e9ec53e8ae947
attakkatutto <attakkatutto@yahoo.com>
2015-06-12 20:25:34 +0200

    sistemato il tempo totale

--- src/aml/agent/JadeManager.java
+++ src/aml/agent/JadeManager.java
@@ -85,10 +85,12 @@ public class JadeManager {
          * an agent can send a message to their neighbour nodes
          */
         for(MyAgent a : agents){
-            SequentialBehaviour seq = new SequentialBehaviour(a);
-            seq.addSubBehaviour(new Sender(a, (MyNode) graph.getNode(a.getLocalName())));
-            seq.addSubBehaviour(new Receiver(a, (MyNode) graph.getNode(a.getLocalName())));
-            a.addBehaviour(seq);
+//            SequentialBehaviour seq = new SequentialBehaviour(a);
+//            seq.addSubBehaviour(new Sender(a, (MyNode) graph.getNode(a.getLocalName())));
+//            seq.addSubBehaviour(new Receiver(a, (MyNode) graph.getNode(a.getLocalName())));
+//            a.addBehaviour(seq);            
+            a.addBehaviour(new Sender(a, (MyNode) graph.getNode(a.getLocalName())));
+            a.addBehaviour(new Receiver(a, (MyNode) graph.getNode(a.getLocalName())));
         }
     }
 
