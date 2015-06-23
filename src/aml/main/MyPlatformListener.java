61b8bd88ab4a08532ad5db744dd0659aab645161
attakkatutto <attakkatutto@yahoo.com>
2015-06-23 14:30:37 +0200

    versione 2 -> vedi documento prof.ssa

--- src/aml/main/MyPlatformListener.java
+++ src/aml/main/MyPlatformListener.java
@@ -8,70 +8,69 @@ package aml.main;
 import jade.wrapper.PlatformController;
 import jade.wrapper.PlatformEvent;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * Custom JADE platform listener 
- * implements interface Listener
- * 
+ * Custom JADE platform listener implements interface Listener
+ *
  * @author ddefalco
  */
-public class MyPlatformListener 
-implements PlatformController.Listener {
-    
-    public MyPlatformListener(MyPlatformManager subject){
-       this.subject = subject;
+public class MyPlatformListener
+        implements PlatformController.Listener {
+
+    public MyPlatformListener(MyPlatformManager subject) {
+        this.subject = subject;
     }
 
     MyPlatformManager subject;
-    List<String> agents = new ArrayList<>();
+    List<String> agents = Collections.synchronizedList(new ArrayList<String>());
 
-                @Override
-                public void deadAgent(PlatformEvent anEvent) {
-                    // WORKS 
-                    String name = anEvent.getAgentGUID();
-                    System.out.println(" - "
-                            + name
-                            + " dead ");
-                    agents.remove(name);
-                    if (agents.isEmpty()) {
-                        System.out.println(" - "
-                                + " JADE end! ");
-                        subject.halt();
-                    }
-                }
+    @Override
+    public void deadAgent(PlatformEvent anEvent) {
+        // WORKS 
+        String name = anEvent.getAgentGUID();
+        System.out.println(" - "
+                + name
+                + " dead ");
+        agents.remove(name);
+        if (agents.isEmpty()) {
+            System.out.println(" - "
+                    + " JADE end! ");
+            subject.halt();
+        }
+    }
 
-                @Override
-                public void bornAgent(PlatformEvent anEvent) {
-                    // WORKS
-                    String name = anEvent.getAgentGUID();
-                    System.out.println(" - "
-                            + name
-                            + " born ");
-                    agents.add(name);
-                }
+    @Override
+    public void bornAgent(PlatformEvent anEvent) {
+        // WORKS
+        String name = anEvent.getAgentGUID();
+        System.out.println(" - "
+                + name
+                + " born ");
+        agents.add(name);
+    }
 
-                @Override
-                public void startedPlatform(PlatformEvent pe) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
+    @Override
+    public void startedPlatform(PlatformEvent pe) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-                @Override
-                public void suspendedPlatform(PlatformEvent pe) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
+    @Override
+    public void suspendedPlatform(PlatformEvent pe) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-                @Override
-                public void resumedPlatform(PlatformEvent pe) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
+    @Override
+    public void resumedPlatform(PlatformEvent pe) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-                @Override
-                public void killedPlatform(PlatformEvent pe) {
-                    System.out.println(" - "
-                            + pe.getPlatformName()
-                            + " killed ");
-                }
+    @Override
+    public void killedPlatform(PlatformEvent pe) {
+        System.out.println(" - "
+                + pe.getPlatformName()
+                + " killed ");
+    }
 
-    
 }
