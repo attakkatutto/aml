adb4c1996df06398af9bf3627acd1e08e033dcab
attakkatutto <attakkatutto@yahoo.com>
2015-10-04 15:41:20 +0200

    refactoring.....

--- src/aml/graph/Network.java
+++ src/aml/graph/Network.java
@@ -8,8 +8,10 @@ package aml.graph;
 import aml.global.Enums.*;
 import aml.global.Config;
 import aml.global.Enums;
+import aml.main.MyOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,6 +19,10 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 import org.graphstream.algorithm.generator.BarabasiAlbertGenerator;
 import org.graphstream.graph.EdgeFactory;
 import org.graphstream.graph.Graph;
@@ -270,6 +276,31 @@ public final class Network extends SingleGraph {
             }
             _index1++;
         }
+    }
+    
+    
+    /**
+     * if GUI is enabled then graph and system.output are rendered in two frame
+     */
+    public void enableGUI() {
+
+        this.display(true);
+
+        JFrame myFrame = new JFrame("SystemMessages");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(700, 400);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+
+        JScrollPane scroll = new JScrollPane(textArea);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        PrintStream printStream = new PrintStream(new MyOutputStream(textArea));
+        System.setOut(printStream);
+        System.setErr(printStream);
+        myFrame.getContentPane().add(scroll);
+        myFrame.setVisible(true);
     }
 
 }
--- src/aml/main/Main.java
+++ src/aml/main/Main.java
@@ -8,11 +8,6 @@ package aml.main;
 import aml.jade.MyPlatformManager;
 import aml.global.Config;
 import aml.graph.Network;
-import java.io.PrintStream;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
 
 /**
  * Main class for the network
@@ -29,34 +24,10 @@ public class Main {
     public static void main(String[] args) {
         Network graph = new Network("AML Synthetic DB");
         if (Config.instance().isGuiEnabled()) {
-            enableGUI(graph);
+            graph.enableGUI();
         }
         graph.build();
         MyPlatformManager f = new MyPlatformManager(graph);
         f.exec();
-    }
-
-    /**
-     * if GUI is enabled then graph and system.output are rendered in two frame
-     */
-    private static void enableGUI(Network graph) {
-
-        graph.display(true);
-
-        JFrame myFrame = new JFrame("SystemMessages");
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.setSize(700, 400);
-
-        JTextArea textArea = new JTextArea();
-        textArea.setEditable(false);
-
-        JScrollPane scroll = new JScrollPane(textArea);
-        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-        PrintStream printStream = new PrintStream(new MyOutputStream(textArea));
-        System.setOut(printStream);
-        System.setErr(printStream);
-        myFrame.getContentPane().add(scroll);
-        myFrame.setVisible(true);
-    }
+    }    
 }
